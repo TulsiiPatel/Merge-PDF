537ae3040982788104c43b1e74a90d830f83ce2f
TulsiiPatel <142133585+TulsiiPatel@users.noreply.github.com>
2023-11-05 17:58:02 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+from PyPDF2 import PdfWriter
+import os
+
+merge = PdfWriter()
+
+files = [file for file in os.listdir() if file.endswith("pdf")]
+for pdf in files:
+    merge.append(pdf)
+
+merge.write("merged_pdf.pdf")
+merge.close()
